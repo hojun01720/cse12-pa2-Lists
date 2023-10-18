8f963f3ba748c0e5c50cf4cb7d39c1cd8ec63b23
hojun01720 <hojun01720@gmail.com>
2023-10-18 07:09:47 -0700

    fdg

--- pa2-starter/src/ArrayGL.java
+++ pa2-starter/src/ArrayGL.java
@@ -9,4 +9,51 @@ public class ArrayGL<E> implements MyList<E> {
     }
 
     // Fill in all required methods here
+
+    public boolean isEmpty(){
+        if(this.size == 0){return true;}
+        return false;
+    }
+
+    public E[] toArray(){
+        E[] newArr = (E[])new Object[this.size];
+        
+        for(int i = 0; i < this.size; i++){
+            newArr[i] = (E) this.elements[i];
+        }
+        return newArr;
+    }
+
+    public void transformAll(MyTransformer mt){
+        for(int i = 0; i < this.size; i++){
+            if(this.elements[i] != null){
+                this.elements[i] = mt.transformElement((E) this.elements[i]);
+            }
+        }
+    }
+
+    public void chooseAll(MyChooser mc){
+
+        int indNum = this.size;
+
+        for(int i = 0; i < this.size; i++){
+            if(this.elements[i] == null || ! mc.chooseElement(this.elements[i]) ) {
+                this.elements[i] = null;
+                indNum --;
+            }
+        }
+
+        E[] newArr = (E[]) new Object[indNum];
+
+        int newArrInd = 0;
+        for(int i = 0; i < this.size; i++){
+            if(this.elements[i] != null){
+                newArr[newArrInd] = this.elements[i];
+                newArrInd ++;
+            }
+        }
+        this.elements = newArr;
+        this.size = this.elements.length;
+
+    }
 }
--- pa2-starter/src/LinkedGL.java
+++ pa2-starter/src/LinkedGL.java
@@ -15,8 +15,99 @@ public class LinkedGL<E> implements MyList<E> {
 
     public LinkedGL(E[] contents) {
         // Fill in this constructor
+        this.size = contents.length;
+
+        this.front = new Node(null, null);
+        Node currentNode = this.front;
+
+        for(int i = 0; i < this.size; i++){
+            currentNode.next = new Node(contents[i], null);
+            currentNode = currentNode.next;
+        }
     }
 
     // Fill in all methods
 
+    public boolean isEmpty(){
+        if (this.size == 0){return true;}
+        return false;
+    }
+
+    public E[] toArray(){
+        E[] newArr = (E[])new Object[this.size];
+
+        Node curNode = this.front; 
+
+        for(int i = 0; i < this.size; i++){
+            newArr[i] = (E) curNode.next;
+        }
+        return newArr;
+    }
+
+    public void transformAll(MyTransformer mt){
+        /*Node oriCurNode = this.front;
+        LinkedGL newLink = this;
+        Node newCurNode = newLink.front;
+
+        E val;
+
+        for(int i = 0; i < this.size; i++){
+            if(oriCurNode.next.value == null){
+                val = null;
+            }
+            else{val = mt.transformElement((E) oriCurNode.next.value);}
+
+            newCurNode.next.value = val;
+
+            oriCurNode = oriCurNode.next;
+            newCurNode = newCurNode.next;
+        }
+        this.front = newLink.front;
+
+        Node oriCurNode = this.front;
+
+        E val;
+
+        for(int i = 0; i < this.size; i++){
+            if(oriCurNode.next.value == null){
+                val = null;
+            }
+            else{val = mt.transformElement((E) oriCurNode.next.value);}
+
+            oriCurNode.next.value = val;
+
+            oriCurNode = oriCurNode.next;
+        }
+        */
+
+        Node oriCurNode = this.front;
+
+        E val;
+
+        for(int i = 0; i < this.size; i++){
+            val = oriCurNode.next.value;
+            if( val != null){
+                oriCurNode.next.value = mt.transformElement((E) val);
+            }
+            oriCurNode = oriCurNode.next;
+        }
+    }
+
+    public void chooseAll(MyChooser mc){
+        Node curNode = this.front;
+        boolean bq;
+        E val;
+
+        for(int i = 0; i < this.size; i++){
+            val = curNode.next.value;
+            if(val == null || ! mc.chooseElement((E) val)){
+                //remove curNode.next node and reconnect the next next one to curNode
+                curNode.next = curNode.next.next;
+                this.size --;
+            }
+            curNode = curNode.next;
+        }
+    }
+
+    
 }
